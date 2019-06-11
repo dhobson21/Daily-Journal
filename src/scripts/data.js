00f81070a407b50955f7e27940675e664ddefe3c
Dustin Hobson <dhobson21@gmail.com>
2019-06-11 09:47:35 -0500

    fixed fetch link

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,6 +1,6 @@
 const API = {
   getJournalEntries: function() {
-    return fetch("http://localhost:8088/entries")
+    return fetch("http://localhost:3000/entries")
     .then(response => response.json()
     );
   }
